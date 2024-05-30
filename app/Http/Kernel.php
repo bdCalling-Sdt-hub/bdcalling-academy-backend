8e08ba0b3f73842d4bbf4d5391a1bf44b3a0e197
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-05-30 15:18:35 +0600

    update Add student

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'super.admin' => \App\Http\Middleware\CheckSuperAdminMiddleware::class,
+        'student' =>\App\Http\Middleware\StudentMiddleware::class,
         'mentor' => \App\Http\Middleware\CheckMentorMiddleware::class,
     ];
 }
---
+++ app/Http/Middleware/StudentMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class StudentMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $user = auth()->userOrFail();
+
+            if ($user->role == 'STUDENT') {
+                return $next($request);
+            }
+
+            return response()->json([
+                'message' => 'Unauthorized user'
+            ], 401);
+
+        } catch (AuthenticationException $exception) {
+            return response()->json([
+                'message' => 'Unauthorized: ' . $exception->getMessage()
+            ], 401);
+        }
+    }
+}
